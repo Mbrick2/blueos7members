8767f5ee288f54e6fc317fb03b1d2ddd6eab6f54
Mbrick2 <90691415+Mbrick2@users.noreply.github.com>
2022-04-07 18:11:40 +1000

    Update JS

---
+++ ci/update.js
@@ -0,0 +1,17 @@
+/* RUNS IN NODEJS ONLY! */
+const fetch = require("cross-fetch");
+const fs = require("fs");
+const id = 31229287; 
+(async() => {
+  let managers = [];
+  let curators = [];
+  const managerReq = await fetch("https://api.scratch.mit.edu/studios/" + id.toString() + "/managers");
+  managers = await managerReq.json();
+  const curatorReq = await fetch("https://api.scratch.mit.edu/studios/" + id.toString() + "/curators");
+  curators = await curatorReq.json();
+  
+  console.log(`${managers.length} managers, ${curators.length} curators`);
+  fs.writeFileSync("./managers.json", JSON.stringify(managers));
+  fs.writeFileSync("./curators.json", JSON.stringify(curators));
+  console.log("written");
+})();
